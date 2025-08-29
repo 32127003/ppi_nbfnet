7081f64261db8a4b4d42c48a68cef00745912be0
32127003 <162433645+32127003@users.noreply.github.com>
2025-08-29 09:14:42 +0900

    Add files via upload

---
+++ nbfnet_pyg_/create_env_nbf.sh
@@ -0,0 +1,7 @@
+pip install torch==1.8.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter==2.0.8 torch-sparse==0.6.12 torch-geometric -f https://data.pyg.org/whl/torch-1.8.0+cu111.html
+pip install ninja easydict pyyaml
+pip install ipykernel
+python -m ipykernel install --user --name nbf37 --display-name "nbf37"
+pip install tqdm==4.66.1
+pip install numpy==1.21.6 pandas==1.1.5
